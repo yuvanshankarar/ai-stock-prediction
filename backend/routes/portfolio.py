f8084a1145931bc6e64a7534b9d4c2d20a4686fc
Yuvan Shankar <yuvanshankarar@gmail.com>
2026-05-29 21:31:31 +0530

    Fix Holding import

--- backend/routes/portfolio.py
+++ backend/routes/portfolio.py
@@ -2,7 +2,11 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from backend.database import SessionLocal
-from backend.models import Balance
+from backend.models import (
+    Balance,
+    Holding,
+    Transaction
+)
 
 router = APIRouter()
 
